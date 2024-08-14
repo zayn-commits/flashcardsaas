e6ea5713b8e57772a23d11887a4bb834efa89590
Zaynerrr <zaynlatchman@gmail.com>
2024-08-14 17:42:24 -0400

    Simple homepage and Correcting get-stripe

--- app/page.js
+++ app/page.js
@@ -1,10 +1,29 @@
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-
+import { AppBar, Container, Toolbar, Typography, Button } from "@mui/material";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    hi
-  )
+    <Container maxWidth="lg">
+      <Head>
+        <title>Flashcard SaaS</title>
+        <meta name = "description" content = 'Create flashcard from your text' />
+      </Head>
+
+      <AppBar position = "static">
+        <Toolbar>
+          <Typography variant = "h6">Flashcard SaaS</Typography>
+          <SignedOut>
+            <Button> Login </Button>
+            <Button>Signup</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+    </Container>
+  );
 }
--- utils/get-stripe.js
+++ utils/get-stripe.js
@@ -2,8 +2,7 @@ import { loadStripe } from "@stripe/stripe-js";
 let stripePromise 
 const getStripe = () =>{
     if(!stripePromise){
-        stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY = pk_live_51PnnslEwcpNez5R6elXXLqKT9rxOQjRpVfojCjQEFiiLTYVnbb81SDfBLeFkHJAglMmFvOC46VloJPhasmL0fxWe00xXBtzRtA
-        )
+        stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
     }
     return stripePromise
 }
