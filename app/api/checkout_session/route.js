6519a3027d03de2b3131bb006f2c9f30c7386d97
Zaynerrr <zaynlatchman@gmail.com>
2024-08-15 17:07:11 -0400

    Pro Checkout Page

--- app/api/checkout_session/route.js
+++ app/api/checkout_session/route.js
@@ -5,17 +5,34 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const formatAmountForStripe = (amount, currency)=>{
     return Math.round(amount * 100)
 }
+
+
+export async function GET(req)
+{
+  const searchParams = req.nextUrl.searchParams
+  const session_id = searchParams.get('session_id')
+
+  try{
+    const checkoutSession = await stripe.checkout.sessions.retrieve(session_id)
+    return NextResponse.json(checkoutSession)
+  }
+  catch(err) {
+    console.error("Error retrieving checkout session:", error)
+    return NextResponse.json({error: {message: error.message}}, {status: 500})
+  }
+}
+
 export async function POST(req) {
 
-const params = {
-    submit_type: 'subscription',
+  const params = {
+    mode: 'subscription',
     payment_method_types: ['card'],
     line_items: [
       {
         price_data: {
             currency: 'usd',
             product_data:{
-                name: 'Pro Subscription'
+                name: 'Pro Subscription', 
             },
             unit_amount: formatAmountForStripe(10),
             recurring:{
@@ -27,8 +44,13 @@ const params = {
         quantity: 1,
       },
     ],
-    success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+    success_url: `${req.headers.get(
+      'origin',
+    )}/result?session_id={CHECKOUT_SESSION_ID}`,
+    
+    cancel_url: `${req.headers.get(
+      'origin',
+    )}/result?session_id={CHECKOUT_SESSION_ID}`,
   };
   const checkoutSession = await stripe.checkout.sessions.create(params);
 
